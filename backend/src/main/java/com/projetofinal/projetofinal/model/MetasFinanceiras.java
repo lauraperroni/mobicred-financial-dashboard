59af5564b8a4872c62831a4ab43f093a90729287
Laura Perroni <laura.silva@unicred.com.br>
2024-03-15 15:22:02 -0300

    model MetasFinanceiras added

---
+++ backend/src/main/java/com/projetofinal/projetofinal/model/MetasFinanceiras.java
@@ -0,0 +1,77 @@
+package com.projetofinal.projetofinal.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class MetasFinanceiras {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Integer idUsuario;
+    private String Descricao;
+    private Double valor;
+    private Date data;
+
+    // Construtores ============================================================
+
+    // Construtor no args
+    public MetasFinanceiras() {
+    }
+
+    // Construtor all args
+    public MetasFinanceiras(Integer id, Integer idUsuario, String descricao, Double valor, Date data) {
+        this.id = id;
+        this.idUsuario = idUsuario;
+        Descricao = descricao;
+        this.valor = valor;
+        this.data = data;
+    }
+
+    // Getters e Setters =======================================================
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        Descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+}
