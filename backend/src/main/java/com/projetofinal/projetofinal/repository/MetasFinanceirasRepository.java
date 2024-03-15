667628391d5191255ef5a2c93f0b6bab8b621c8e
Laura Perroni <laura.silva@unicred.com.br>
2024-03-15 15:29:28 -0300

    repository MetasFinanceirasRepository added

---
+++ backend/src/main/java/com/projetofinal/projetofinal/repository/MetasFinanceirasRepository.java
@@ -0,0 +1,8 @@
+package com.projetofinal.projetofinal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.projetofinal.projetofinal.model.MetasFinanceiras;
+
+public interface MetasFinanceirasRepository extends JpaRepository<MetasFinanceiras, Integer> {
+
+}
