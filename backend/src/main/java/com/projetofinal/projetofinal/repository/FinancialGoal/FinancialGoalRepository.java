2d834f0364c8737e1f8caf92796dd918a2fd2dbf
Laura Perroni <laura.silva@unicred.com.br>
2024-03-20 11:45:43 -0300

    Fixer FinancialGoalRepository annotation

--- backend/src/main/java/com/projetofinal/projetofinal/repository/FinancialGoal/FinancialGoalRepository.java
+++ backend/src/main/java/com/projetofinal/projetofinal/repository/FinancialGoal/FinancialGoalRepository.java
@@ -1,8 +1,11 @@
 package com.projetofinal.projetofinal.repository.FinancialGoal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.projetofinal.projetofinal.model.FinancialGoal;
 
+@Repository
 public interface FinancialGoalRepository extends JpaRepository<FinancialGoal, Integer> {
 
 }
