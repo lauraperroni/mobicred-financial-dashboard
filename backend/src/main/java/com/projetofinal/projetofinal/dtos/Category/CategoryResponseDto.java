e8e05202c31b5882dc705dea7ee1d57dcbbc9dec
Laura Perroni <laura.silva@unicred.com.br>
2024-03-25 14:30:53 -0300

    created GET All Dto method for category

--- backend/src/main/java/com/projetofinal/projetofinal/dtos/Category/CategoryResponseDto.java
+++ backend/src/main/java/com/projetofinal/projetofinal/dtos/Category/CategoryResponseDto.java
@@ -12,17 +12,6 @@ public class CategoryResponseDto {
     public CategoryResponseDto() {
     }
 
-    // Construtor all args
-    public CategoryResponseDto(Integer id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    // Construtor sem o id
-    public CategoryResponseDto(String name) {
-        this.name = name;
-    }
-
     // Construtor usando Category como args
     public CategoryResponseDto(Category category) {
         id = category.getId();
--- backend/src/main/java/com/projetofinal/projetofinal/service/CategoryService.java
+++ backend/src/main/java/com/projetofinal/projetofinal/service/CategoryService.java
@@ -1,10 +1,13 @@
 package com.projetofinal.projetofinal.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
+import com.projetofinal.projetofinal.dtos.Category.CategoryResponseDto;
 import com.projetofinal.projetofinal.model.Category.Category;
 import com.projetofinal.projetofinal.repository.Category.CategoryRepository;
 
@@ -13,34 +16,52 @@ public class CategoryService {
 
     // Repositório que vai conversar com o banco de dados
     @Autowired
-    private CategoryRepository repository;
+    private CategoryRepository categoryRepository;
 
     // Métodos que os Endpoints usam ============================================
 
     // Trazer todas as categorias ===============================================
     public List<Category> getAllCategoryService() {
-        return repository.findAll();
+        return categoryRepository.findAll();
+    }
+
+    // Trazer todas as categorias Dto ===========================================
+    public List<CategoryResponseDto> getAllCategoryDtoService() {
+        List<Category> categories = categoryRepository.findAll();
+        return mapCategoryListToCategoryDtoList(categories);
+    }
+
+    private List<CategoryResponseDto> mapCategoryListToCategoryDtoList(List<Category> categories) {
+        return categories.stream()
+                .map(this::mapCategoryToCategoryDtoService)
+                .collect(Collectors.toList());
+    }
+
+    // Converte uma accountBancaria em uma accountBancariaDto ===============
+    private CategoryResponseDto mapCategoryToCategoryDtoService(Category category) {
+        CategoryResponseDto dto = new CategoryResponseDto(category);
+        return dto;
     }
 
     // Traz uma categoria pelo id ================================================
     @SuppressWarnings("null")
     public Category getCategoryIdService(Integer id) {
-        return repository.findById(id).get();
+        return categoryRepository.findById(id).get();
     }
 
     // Adicionar novo usuário =================================================
     @SuppressWarnings("null")
     public ResponseEntity<String> postNewCategoryService(Category category) {
-        repository.save(category);
+        categoryRepository.save(category);
         return ResponseEntity.ok("New category created.");
     }
 
     // Update de um usuário por id ============================================
     @SuppressWarnings("null")
     public ResponseEntity<String> putUpdateCategoryService(Integer id, Category category) {
-        if (repository.existsById(id)) {
+        if (categoryRepository.existsById(id)) {
             category.setId(id);
-            repository.save(category);
+            categoryRepository.save(category);
             return ResponseEntity.ok("Category updated.");
         } else {
             return ResponseEntity.status(404).body("Category not found.");
@@ -50,8 +71,8 @@ public class CategoryService {
     // Deletar um usuário por id =============================================
     @SuppressWarnings("null")
     public ResponseEntity<String> deleteCategoryIdService(@PathVariable Integer id) {
-        if (repository.existsById(id)) {
-            repository.deleteById(id);
+        if (categoryRepository.existsById(id)) {
+            categoryRepository.deleteById(id);
             return ResponseEntity.ok("Category created.");
         } else {
             return ResponseEntity.status(404).body("Category not found.");
