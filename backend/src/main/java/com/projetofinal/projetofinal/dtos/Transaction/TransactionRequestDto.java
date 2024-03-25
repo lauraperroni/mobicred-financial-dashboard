b680ca13641bb4dc2a0f7637cd71b9c6793f0399
Laura Perroni <laura.silva@unicred.com.br>
2024-03-25 14:04:04 -0300

    added name to Transaction's GET + optimized POST

--- backend/src/main/java/com/projetofinal/projetofinal/dtos/Transaction/TransactionRequestDto.java
+++ backend/src/main/java/com/projetofinal/projetofinal/dtos/Transaction/TransactionRequestDto.java
@@ -3,8 +3,9 @@ package com.projetofinal.projetofinal.dtos.Transaction;
 import java.sql.Date;
 
 public record TransactionRequestDto(
-                Double amount,
-                Date date,
-                Integer categoryId,
-                Integer bankAccountId) {
+        Double amount,
+        Date date,
+        Integer categoryId,
+        String categoryName,
+        Integer bankAccountId) {
 }
